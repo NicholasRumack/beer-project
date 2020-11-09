ca32f00b76f3d1e47ecc2057b15fd88ef48de493
Trollungen85 <70262395+Trollungen85@users.noreply.github.com>
2020-11-09 09:53:22 +0100

    sökresultat fixat

--- search-page.js
+++ search-page.js
@@ -4,33 +4,35 @@ const sectionElement = document.getElementById('searchresults');
 const first = document.querySelector(".first");
 const previous = document.querySelector(".previous");
 const next = document.querySelector(".next");
+let number = 1;
 
-const page = "&page=" + pageValue;
-let pageValue = 1;
-
+//const page = "&page=" + 'pageValue';
+//let pageValue = 1;
 formElement.addEventListener('submit', onSubmit);
-
-
+ 
 function onSubmit(evt) {
     
     const searchStr = evt.target[0].value;
-    const url = `${api}?beer_name=${searchStr}${page}&per_page=10`;
+    
+ 
+    url = `${api}?beer_name=${searchStr}&page=${number}&per_page=10`;
+    //const url = `${api}?beer_name=${searchStr}${page}&per_page=10`;
     //const url = api + "?beer_name" + searchStr; 
     //samma syntax som raden ovan
-
+ 
     getData(url, renderFirstBeer);
     evt.preventDefault();
     removeAllChildNodes(sectionElement);
     console.log(url);
 }
-
+ 
 function removeAllChildNodes(parent) {
     
     while (parent.firstChild) {      
         parent.removeChild(parent.firstChild);
     }
 }
-
+ 
 function getData(url, callback) {
     fetch(url)
     .then(res => res.json())
@@ -38,33 +40,32 @@ function getData(url, callback) {
         callback(data);
     })
     .catch(error => console.log(error));
-    
 }
-
+ 
 function renderFirstBeer(data) {
     
     const ulElement = document.createElement('ul');
-
+ 
     for (let i = 0; i < data.length; i++) {
        
         const beer = data[i];
         const liElement = document.createElement('li');
         liElement.setAttribute('name', beer.id)
         liElement.textContent = beer.name;
-
+ 
         ulElement.appendChild(liElement); //lägger li i ul med appendchild
         
     }
     
     sectionElement.appendChild(ulElement);
-
+ 
 }
-
+ 
 /*
 function onUlClicked(evt) {
     
     const id = evt.target.getAttribute('name');
-
+ 
     const url = `./myView.html?name=${id}`;
     document.location.href = url;
 }
