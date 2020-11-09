e97fb893b1ec9dc43b474f38e9ba806d431362bb
Trollungen85 <70262395+Trollungen85@users.noreply.github.com>
2020-11-09 13:56:00 +0100

    Fixar med random sidan, jag är INTE klar med javascripten

--- landing-page.js
+++ landing-page.js
@@ -1,10 +1,34 @@
-const api = 'https://api.punkapi.com/v2/beers';
+const api = `https://api.punkapi.com/v2/beers/random`;
+const randomBtn = document.querySelector ("random")
+const card = document.querySelector ("randomCard")
 
-function getData(url, callback) {
+function getData(url, callback) {  // hämtar api och gör om till json
     fetch(url)
     .then(res => res.json())
     .then(data => {
-        callback(data);
+    // console.log(data)
+    // callback(data);
     })
     .catch(error => console.log(error));
+}
+
+randomBtn.addEventListener('click', randomClick);
+
+function randomClick(data) {
+    
+    const ulElement = document.createElement('div');
+ 
+    for (let i = 0; i < data.length; i++) {
+       
+        const beer = data[i];
+        const liElement = document.createElement('h2');
+        liElement.setAttribute('name', beer.id)
+        liElement.textContent = beer.name;
+ 
+        ulElement.appendChild(liElement); 
+        
+    }
+    
+    sectionElement.appendChild(ulElement);
+    card.appendChild(ulElement)
 }
