5a9d5de55e065294cb383cd9ae6a1c5bda0a5d81
Flure1 <Flure1@users.noreply.github.com>
2020-11-11 17:19:25 +0100

    nu ligger varje info i varsin div

--- info-page.js
+++ info-page.js
@@ -1,10 +1,8 @@
 const api = 'https://api.punkapi.com/v2/beers';
-const detailsElement = document.querySelector('section.beerInfoGrid > div');
+const detailsElement = document.querySelector('section.beerInfoGrid');
 const searchParams = new URLSearchParams(window.location.search);
 const id = searchParams.get('name');
 const url = `${api}/${id}`;
-
-let beer = [];
 
 getData(url, getBeerInfo)
 
@@ -27,60 +25,84 @@ function getBeerInfo(data) {
     const pElement = document.createElement('p');
     pElement.innerText = str;
 
-    /*pElement.onclick = function() {
-        console.log('beer data >>>' + JSON.stringify(beer));
-        removeAllChildNodes(detailsElement);
-    */
-        const image_url = beer.image_url;
-        const abv = beer.abv;
-        const volume = beer.volume;
-        const hops = beer.hops;
-        const description = beer.birth_year;
-        const ingredients = beer.ingredients;
-        const food_pairing = beer.food_pairing;
-        const brewers_tips = beer.brewers_tips;
+    const image = "" + beer.image_url;
+    const abv = "Alcohole by volume: " + beer.abv;
+    const volume = "Volume: " + beer.volume;
+    const hops = "Hops: " + beer.hops;
+    const description = "Description: " + beer.description;
+    const ingredients = "Ingredients: " + beer.ingredients;
+    const food_pairing = "Food pairing: " + beer.food_pairing;
+    const brewers_tips = "Brewers tips: " + beer.brewers_tips;
 
-        const imgTagbeerImg  = document.createElement('img');
-        const h2Tag  = document.createElement('h2');
-        const pTagAlcoholeByVolume  = document.createElement('p');
-        const pTagVolume  = document.createElement('p');
-        const pTagHops  = document.createElement('p');
-        const pTagDescription  = document.createElement('p');
-        const pTagIngredients  = document.createElement('p');
-        const pTagFoodPairing  = document.createElement('p');
-        const pTagBrewersTips  = document.createElement('p');
+    const imgTagbeerImg  = document.createElement('img');
+    const h2Tag  = document.createElement('h2');
+    const pTagAlcoholeByVolume  = document.createElement('p');
+    const pTagVolume  = document.createElement('p');
+    const pTagHops  = document.createElement('p');
+    const pTagDescription  = document.createElement('p');
+    const pTagIngredients  = document.createElement('p');
+    const pTagFoodPairing  = document.createElement('p');
+    const pTagBrewersTips  = document.createElement('p');
 
-        const image_urlNode = document.createTextNode('Image:' + ' ' + image_url);
-        const h2Node = document.createTextNode(str);
-        const pAlcoholeByVolumeNode = document.createTextNode('Alcohol by volume:' + ' ' + abv);
-        const pVolumeNode = document.createTextNode('Volume:' + ' ' + volume);
-        const pHopsNode = document.createTextNode('Hops:' + ' ' + hops);
-        const pDescriptionNode = document.createTextNode('Description:' + ' ' + description);
-        const pIngredientsNode = document.createTextNode('Ingredients:' + ' ' + ingredients);
-        const pFoodPairingNode = document.createTextNode('Food pairing:' + ' ' + food_pairing);
-        const pBrewersTipsNode = document.createTextNode('Brewers tips:' + ' ' + brewers_tips);
+    const image_urlNode = document.createTextNode(image);
+    const h2Node = document.createTextNode(str);
+    const pAlcoholeByVolumeNode = document.createTextNode(abv);
+    const pVolumeNode = document.createTextNode(volume);
+    const pHopsNode = document.createTextNode(hops);
+    const pDescriptionNode = document.createTextNode(description);
+    const pIngredientsNode = document.createTextNode(ingredients);
+    const pFoodPairingNode = document.createTextNode(food_pairing);
+    const pBrewersTipsNode = document.createTextNode(brewers_tips);
 
-        imgTagbeerImg.appendChild(image_urlNode);
-        h2Tag.appendChild(h2Node);
-        pTagAlcoholeByVolume.appendChild(pAlcoholeByVolumeNode);
-        pTagVolume.appendChild(pVolumeNode);
-        pTagHops.appendChild(pHopsNode);
-        pTagDescription.appendChild(pDescriptionNode);
-        pTagIngredients.appendChild(pIngredientsNode);
-        pTagFoodPairing.appendChild(pFoodPairingNode);
-        pTagBrewersTips.appendChild(pBrewersTipsNode);
+    imgTagbeerImg.appendChild(image_urlNode);
+    h2Tag.appendChild(h2Node);
+    pTagAlcoholeByVolume.appendChild(pAlcoholeByVolumeNode);
+    pTagVolume.appendChild(pVolumeNode);
+    pTagHops.appendChild(pHopsNode);
+    pTagDescription.appendChild(pDescriptionNode);
+    pTagIngredients.appendChild(pIngredientsNode);
+    pTagFoodPairing.appendChild(pFoodPairingNode);
+    pTagBrewersTips.appendChild(pBrewersTipsNode);
 
-        detailsElement.appendChild(h2Tag);
-        detailsElement.appendChild(pTagAlcoholeByVolume);
-        detailsElement.appendChild(pTagVolume);
-        detailsElement.appendChild(pTagHops);
-        detailsElement.appendChild(pTagDescription);
-        detailsElement.appendChild(pTagIngredients);
-        detailsElement.appendChild(pTagFoodPairing);
-        detailsElement.appendChild(pTagBrewersTips);
+    const divBeerImg = document.createElement('div');
+    divBeerImg.classList.add('beerImg');
+    const divBeerName = document.createElement('div');
+    divBeerName.classList.add('beerName');
+    const divAlcoholByVolume = document.createElement('div');
+    divAlcoholByVolume.classList.add('alcoholByVolume');
+    const divVolume = document.createElement('div');
+    divVolume.classList.add('volume');
+    const divHops = document.createElement('div');
+    divHops.classList.add('hops');
+    const divBeerDesc = document.createElement('div');
+    divBeerDesc.classList.add('beerDesc');
+    const divBeerInfo = document.createElement('div');
+    divBeerInfo.classList.add('beerInfo');
+    const divfoodPairing = document.createElement('div');
+    divfoodPairing.classList.add('foodPairing');
+    const divBrewersTips = document.createElement('div');
+    divBrewersTips.classList.add('brewersTips');
 
+    divBeerImg.appendChild(imgTagbeerImg);
+    divBeerName.appendChild(h2Tag);
+    divAlcoholByVolume.appendChild(pTagAlcoholeByVolume);
+    divVolume.appendChild(pTagVolume);
+    divHops.appendChild(pTagHops);
+    divBeerDesc.appendChild(pTagDescription);
+    divBeerInfo.appendChild(pTagIngredients);
+    divfoodPairing.appendChild(pTagFoodPairing);
+    divBrewersTips.appendChild(pTagBrewersTips);
 
-        divElement.appendChild(pElement);
+    detailsElement.appendChild(divBeerImg);
+    detailsElement.appendChild(divBeerName);
+    detailsElement.appendChild(divAlcoholByVolume);
+    detailsElement.appendChild(divVolume);
+    detailsElement.appendChild(divHops);
+    detailsElement.appendChild(divBeerDesc);
+    detailsElement.appendChild(divBeerInfo);
+    detailsElement.appendChild(divfoodPairing);
+    detailsElement.appendChild(divBrewersTips);
+    
     }
 
     
